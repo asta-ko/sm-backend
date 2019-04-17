c8148a32320a875838d64ab8cc986bc5282db371
Asta Ko <asta.ko@ovdinfo.org>
2019-04-17 15:16:21 +0300

    Better case list admin page

--- oi_sud/cases/admin.py
+++ oi_sud/cases/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import Case, CaseEvent, CaseGroup, Defendant, CaseDefense
 
 from jet.admin import CompactInline
+from jet.filters import RelatedFieldAjaxListFilter
 
 class DefendantsInline(CompactInline):
     model = CaseDefense
@@ -16,8 +17,8 @@ class CaseEventsInline(CompactInline):
 
 class CaseAdmin(admin.ModelAdmin):
     inlines = (DefendantsInline, CaseEventsInline)
-    list_filter = ('court', 'court__region', 'stage', 'type', 'result_type')
-    list_display = ('__str__', 'court', 'judge', 'entry_date', 'result_date', 'result_type' )
+    list_filter = (('court', RelatedFieldAjaxListFilter), 'court__region', 'stage', 'type', 'result_type')
+    list_display = ('__str__', 'court', 'judge',  'result_type', 'entry_date', 'result_date')
     search_fields = ('title', 'city', 'full_address',)
     #raw_id_fields = ('codex_articles',)
 
