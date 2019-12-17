a051dd8a7b747aa52533ebbd74bfa0253debf56f
Asta Kovrova <ksawie@gmail.com>
2019-12-17 18:39:14 +0300

    Oops

--- oi_sud/config/default.py
+++ oi_sud/config/default.py
@@ -67,7 +67,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "versioning.middleware.VersioningMiddleware",
 ]
 
 ROOT_URLCONF = 'oi_sud.urls'
