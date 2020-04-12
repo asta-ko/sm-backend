e712d91a60f074586c80af33cabf7dc20d27613b
mi <mansurova.m@mail.ru>
2020-04-12 23:25:03 +0300

    small fix in get_compulsory_works

--- oi_sud/cases/parsers/result_texts.py
+++ oi_sud/cases/parsers/result_texts.py
@@ -164,7 +164,9 @@ class KoapPenaltyExtractor(object):
         patterns = [re.compile(
             r'в? виде обязательных работ (на(?! срок)|на срок|сроком ?н?а?(в )?)?(?P<works_data>.{,40})'
             r'(час(а|ов)|>|\*|&gt;|\.\.|--)'),
-            re.compile(r'в виде (?P<works_data>.{,40})(час(а|ов)) обязательных работ')]
+            re.compile(r'в виде (?P<works_data>.{,40})(час(а|ов)) обязательных работ'),
+            re.compile(r'наказание - (?P<works_data>.{,40})(час(а|ов)) обязательных работ')
+        ]
         works_hours = None
         not_found = True
         hidden = False
